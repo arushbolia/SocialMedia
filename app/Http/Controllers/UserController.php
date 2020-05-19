6f577ff0ad24b781cd6aa7cecb93c2f38d81688e
Arush Bolia <boliaarush@gmail.com>
2020-05-19 08:49:45 +0530

    Input Validation Done in both sign in and up pages

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,6 +16,13 @@ class UserController extends Controller
 
     public function postSignUp(Request $request)
     {
+
+        $this->validate($request, [
+            'email' => 'required|email|unique:users',
+            'first_name' => 'required|max:120',
+            'password' => 'required|min:4|max:16'
+        ]);
+
         $email = $request['email'];
         $first_name = $request['first_name'];
         $password = bcrypt($request['password']);
@@ -28,13 +35,19 @@ class UserController extends Controller
         $user->save();
 
         Auth::login($user);
-        
+
         return redirect()->route('dashboard');
 
     }
 
     public function postSignIn(Request $request)
     {
+
+        $this->validate($request, [
+            'email' => 'required',
+            'password' => 'required'
+        ]);  
+
         $email = $request['email'];
         $password = $request['password'];
         if(Auth::attempt(['email' => $email, 'password' => $password]))
